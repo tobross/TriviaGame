34a3eb801a6db091921cd8fb7aeb2a755deaf882
Tobey Ross <tobross@gmail.com>
2018-04-25 22:34:17 -0500

    added functionality, timers, and results page. Reset button non-functional.

---
+++ assets/javascript/app.js
@@ -0,0 +1,105 @@
+var newGame = $(document).ready(function() {
+//Create array of pages.
+var pages = [".question1", ".question2", ".question3", ".question4", ".question5", ".question5", ".question6", ".question7", ".question8", ".question9", ".question10"];
+for (i = 0; i < pages.length; i++) {}
+var correct = 0;
+var incorrect = 0;
+var round = 1;
+var timer;
+var counter;
+var second;
+var game =$(document).on("click", ".start", function() {
+    $(".titlePage").addClass("hide");
+    $(".question1").removeClass("hide");
+    $(".counter").removeClass("hide");
+    clearInterval(trackTime);
+    clearInterval(quizTime);
+    trackTime();
+    counter = 20
+    timer = setInterval(quizTime, 20000);
+});
+function trackTime() {
+    clearInterval(trackTime)
+    clearInterval(quizTime);
+    $("#counter").text(counter);
+    counter--;
+};
+second = setInterval(trackTime, 1000);
+
+function quizTime() {
+    $(".counter").removeClass("hide");
+    counter = 20;
+    clearInterval(timer);
+    clearInterval(second);
+    timer = setInterval(quizTime, 20000);
+    second = setInterval(trackTime, 1000);
+    $(".question" + round).addClass("hide");
+    round++;
+    $(".question" + round).removeClass("hide");
+    incorrect++;
+    console.log(incorrect);
+    if (round === 11) {
+        clearInterval(timer);
+        clearInterval(second);
+
+    };
+};
+$(".answers").on("click", ".ans", function() {
+    $(".counter").removeClass("hide");
+    counter = 20;
+    clearInterval(timer);
+    clearInterval(second);
+    timer = setInterval(quizTime, 20000);
+    second = setInterval(trackTime, 1000);
+    $(".question" + round).addClass("hide");
+    round++;
+    $(".question" + round).removeClass("hide");    
+    if (round === 11) {
+        clearInterval(second);
+        clearInterval(timer);
+    }
+    else if ($(this).hasClass("correct")) {
+        correct++;
+        console.log(correct + " correct");
+        correct = correct;
+    }
+    else {
+        incorrect++;
+        console.log(incorrect + " incorrect");
+        incorrect = incorrect;
+    }
+    if (correct >= 6) {
+        $(".winLose").html("<img class='questionPic' src='assets/images/dndvictory.png' alt='victorypic'>");
+        $("#passFail").text("Passed");
+        $(".progressBar").attr("aria-valuenow", correct).css("width", correct);
+        $(".ratio").text("You answered " + correct + " out of 10 correctly! We feast tonight!");
+    }
+    else {
+        $(".winLose").html("<img class='questionPic' src='assets/images/dndtpk.png' alt='tpk'>");
+        $("#passFail").text("Failed");
+        $(".progressBar").attr("aria-valuenow", correct).css("width", correct);
+        $(".ratio").text("You answered " + correct + " out of 10 correctly! Do better!");
+}})
+
+// var answer = $(".answers").on("click", "btn", function() {
+//     if (answer = ".correct") {
+//         correct++;
+//     }
+// })
+//Display opening page with title, instructions, and start button.
+
+
+//after start button is pressed, hide start page and display first question, image for question, answers with selectable buttons, and timer.
+
+
+//if a selection is made, hide question, reset timer, and display next question and image. Also increment correct or incorrect by 1.
+
+
+
+//if a selection is not made by the end of timeOut, increment incorrect by 1, hide question, and display the next question.
+
+
+//After all 10 questions have been answered or timed out, display end screen with "loading bar" showing percentage correct, number of questions correct out of total
+
+
+})
